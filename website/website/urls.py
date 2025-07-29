c8355137d5e7aa307b2c3bc0837f2547df95abff
Anna Suslova <suslova.as@phystech.edu>
2025-07-30 00:13:55 +0300

    Add an app called service for communications with users

--- website/website/urls.py
+++ website/website/urls.py
@@ -23,5 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('profile/', include('users.urls'))
+    path('profile/', include('users.urls')),
+    path('service/', include('service.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
